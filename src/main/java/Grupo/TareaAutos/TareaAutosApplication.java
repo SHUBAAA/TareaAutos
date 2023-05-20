279aadaadea6ed0f15c4db37ad7d56d186e27d7b
SHUBAAA <m.alvial03@ufromail.cl>
2023-05-20 19:32:17 -0400

    hola mundo

---
+++ src/main/java/Grupo/TareaAutos/TareaAutosApplication.java
@@ -0,0 +1,13 @@
+package Grupo.TareaAutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaAutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaAutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Grupo/TareaAutos/TareaAutosApplicationTests.java
@@ -0,0 +1,13 @@
+package Grupo.TareaAutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaAutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
