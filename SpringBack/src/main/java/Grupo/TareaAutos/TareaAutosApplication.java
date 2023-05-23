997454a14b1f6d1e815a2cfd8dc51ae644ca4e41
SHUBAAA <m.alvial03@ufromail.cl>
2023-05-22 23:49:09 -0400

    Mezclar front con back

--- SpringBack/src/main/java/Grupo/TareaAutos/TareaAutosApplication.java
+++ SpringBack/src/main/java/Grupo/TareaAutos/TareaAutosApplication.java
@@ -5,17 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TareaAutosApplication {
-
-
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(TareaAutosApplication.class, args);
 	}
-
-
-
-
-
 
 }
